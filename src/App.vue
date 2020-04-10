ca81285e55e8c304678c238c9631c8e872caed65
hectortllo <hectortllo@gmail.com>
2020-04-09 18:56:57 -0600

    Se comenzó a integrar todo lo aprendido en la aplicación del curso

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
+    section.section 
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(
+            type="text", 
+            placeholder = "Buscar Canciones", 
+            v-model="searchQuery"
+          )
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+          p
+            small {{ searchMessage }}
+
+      .container.results
+        .columns
+          .column(v-for="t in tracks") {{ t.name }} - {{ t.artist }}
+    
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+const tracks = [
+  { name: "Muchacha", artist: "Luis Alberto Spineta" },
+  { name: "Hoy acá en el baile", artist: "El Pepo" },
+  { name: "I was made for loving you", artist: "Kiss" }
+];
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      searchQuery: "",
+      tracks: []
+    };
+  },
+
+  methods: {
+    search() {
+      this.tracks = tracks;
+    }
+  },
+
+  computed: {
+    searchMessage() {
+      return `Encontrados: ${this.tracks.length}`;
+    }
   }
 };
 </script>
 
 <style lang="scss">
 @import "./scss/main.scss";
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.results {
+  margin-top: 50px;
 }
 </style>
