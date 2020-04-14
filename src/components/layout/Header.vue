96aa2453a5034fde025e5064ce98f76d05c6941a
hectortllo <hectortllo@gmail.com>
2020-04-13 20:39:36 -0600

    Se agregó la funcionalidad de la comunicación entre componentes, aún sin tener una relación directa de hijo a padre o viceversa

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -12,4 +12,13 @@
       .container.has-text-centered
         h1.title Platzi Music
         h2.subtitle Canciones que están Buenísimas!
+        pm-player
 </template>
+
+<script>
+  import PmPlayer from '@/components/Player.vue'
+
+  export default {
+    components: { PmPlayer }
+  }
+</script>
