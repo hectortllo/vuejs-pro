ce45d92b2eb359b088bb24fb8db34edf587e2c26
hectortllo <hectortllo@gmail.com>
2020-04-14 21:11:13 -0600

    Se agregó el vue-router a la aplicación

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from "vue";
+import VueRouter from 'vue-router';
 import App from "@/App.vue";
+
+import routes from '@/routes'
 
 import EventBus from '@/plugins/event-bus'
 
 Vue.use(EventBus)
+Vue.use(VueRouter)
 
+const router = new VueRouter({ routes })
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
